f00f56200a1b486e06f1817b8efb2bf123232c08
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-28 06:12:27 +0700

    Initial Commit

---
+++ PoSSwiftUI/PoSSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoSSwiftUIApp.swift
+//  PoSSwiftUI
+//
+//  Created by MacBook Pro on 28/04/23.
+//
+
+import SwiftUI
+
+@main
+struct PoSSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

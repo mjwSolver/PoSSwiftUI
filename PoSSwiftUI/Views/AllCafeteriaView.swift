b51af4cf2e367632cfaefd84693bdfe310855474
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-04-28 08:19:13 +0700

    The First View

---
+++ PoSSwiftUI/Views/AllCafeteriaView.swift
@@ -0,0 +1,20 @@
+//
+//  AllCafeteriaView.swift
+//  PoSSwiftUI
+//
+//  Created by MacBook Pro on 28/04/23.
+//
+
+import SwiftUI
+
+struct AllCafeteriaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AllCafeteriaView_Previews: PreviewProvider {
+    static var previews: some View {
+        AllCafeteriaView()
+    }
+}

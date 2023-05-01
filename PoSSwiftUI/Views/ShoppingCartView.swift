6cd7ef4f7d196c25a905f39a40758c48fe6b9607
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-05-01 21:46:09 +0700

    Creating ShoppingCart

---
+++ PoSSwiftUI/Views/ShoppingCartView.swift
@@ -0,0 +1,20 @@
+//
+//  ShoppingCartView.swift
+//  PoSSwiftUI
+//
+//  Created by MacBook Pro on 01/05/23.
+//
+
+import SwiftUI
+
+struct ShoppingCartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShoppingCartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShoppingCartView()
+    }
+}

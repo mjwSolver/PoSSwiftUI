f38ef40100d6f40e7b48a3c1213f48f76c7260fd
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-05-02 22:28:20 +0700

    Creating ShoppingCart as a Class

---
+++ PoSSwiftUI/Models/ShoppingCart.swift
@@ -0,0 +1,62 @@
+//
+//  ShoppingCart.swift
+//  PoSSwiftUI
+//
+//  Created by MacBook Pro on 02/05/23.
+//
+
+import Foundation
+
+class ShoppingCart {
+    
+   var contents: [CartItem] = []
+    
+    init() {}
+    
+    func isSimilarItemInCartOf(id: Int) -> Bool{
+        
+        for theItems in contents {
+            if theItems.id == id {
+                return true
+            } else { continue }
+        }
+        
+        return false;
+        
+    }
+    
+    func retrieveItemInCart(id: Int) -> Int {
+        
+        if !isSimilarItemInCartOf(id: id) {
+//            return CartItem(id: -1, name: "Blank", price: 0, quantity: 0)
+            return -1
+        }
+        
+        for (theIndex, theItems) in contents.enumerated() {
+            if theItems.id == id {
+                return theIndex
+            } else { continue }
+        }
+            
+//        return CartItem(id: -1, name: "Duplicate ID", price: 0, quantity: 0)
+        return -1
+        
+    }
+    
+    func extractAllStoreNames() -> [String] {
+        
+        var AllTheStores: [String] = []
+        
+        for theItems in contents {
+      
+            if !AllTheStores.contains( theItems.storeName ) {
+                AllTheStores.append( theItems.storeName )
+            }
+            
+        }
+        
+        return AllTheStores
+    }
+    
+}
+

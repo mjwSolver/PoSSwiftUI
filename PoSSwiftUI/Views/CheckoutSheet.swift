6c17db730f812ba87bde0a60912bcd3f9de9957b
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-05-03 01:03:16 +0700

    Creating CheckoutSheet
    Still confused how to back out twice

--- PoSSwiftUI/Views/CheckoutSheet.swift
+++ PoSSwiftUI/Views/CheckoutSheet.swift
@@ -9,16 +9,69 @@ import Foundation
 import SwiftUI
 
 struct CheckoutSheet: View {
+//    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @Environment(\.dismiss) var dismiss
+    @EnvironmentObject var theModelData: ModelData
+    
+    @Binding var paymentSucceeded: Bool
+    
+    var theCart:ShoppingCart {
+        theModelData.theShoppingCart
+    }
+    
+    @State private var userPayment = ""
 
     var body: some View {
-        Button("Press to dismiss") {
-            dismiss()
+        VStack {
+            let theFinalBill = theCart.calculateTotalBill()
+            Text("Total Bill")
+                .font(.title)
+            Text("Rp. \(theFinalBill),00-")
+                .font(.subheadline)
+            
+            TextField("Your payment", text: $userPayment)
+                .multilineTextAlignment(.center)
+                .padding(.bottom)
+             
+            Button("Pay"){
+                if processPayment(finalBill: theFinalBill) {
+                    paymentSucceeded.toggle()
+                    dismiss()
+//                    self.presentationMode.wrappedValue.dismiss()
+
+                }
+            }
+            
+            HStack{
+                Text("Swipe down to cancel")
+                    .padding(.top)
+//                Button("Back") { dismiss() }
+//                    .padding()
+            }
+
         }
-        .font(.title)
-        .padding()
-        .background(.black)
     }
+    
+    func processPayment(finalBill theFinalBill:Int)-> Bool {
+        
+        let thePayment = Int(userPayment) ?? -1
+        let isValid = thePayment != -1
+        
+        if !isValid {
+            return false
+        }
+                
+        if thePayment <= 0 {
+            return false
+        }
+        
+        if thePayment < theFinalBill {
+            return false
+        }
+        
+        return true
+    }
+    
 }
 
 
